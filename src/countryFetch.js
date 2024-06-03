1a6e15460b76debf162dfc46fbc96e9a11ba626a
Miguel <miguelsribeiro7@gmail.com>
2024-06-03 02:14:18 +0100

    fix

--- src/countryFetch.js
+++ src/countryFetch.js
@@ -1,9 +1,6 @@
-// 1e52812f9bd9a0d90f03d649a3220787
-// http://api.openweathermap.org/data/2.5/weather?q=Portugal&appid=YOUR_API_KEY
-
 const fetchWeather = async (country) => {
   const response = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${country}&appid=1e52812f9bd9a0d90f03d649a3220787&units=metric`
+    `https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${import.meta.env.VITE_API}&units=metric`
   );
   const data = await response.json();
 
